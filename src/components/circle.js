8382e6bad97d774b104832df9660db3a5faca824
Ayushi Sharma <sharmaroliayushi@gmail.com>
2020-11-24 14:00:50 +0530

    final

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,15 @@
 
 
 // export class using module.exports
+const Shape = require('./shape');
+class Circle extends Shape {
+    constructor(color) {
+        super(color);
+
+    }
+    calculateArea() {
+        console.log("Area of circle with color: " +  this.color);
+        return 3.14*5*5;
+    }
+}
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+
+  constructor() {
+      this.color = "red";
+  }
+  drawShape() {
+      console.log("Drawing Shape");
+  }
+  calculateArea() {
+      console.log("Dont know area of unknown shape");
+      return 0;
+  }
+}
+module.exports = Shape;
